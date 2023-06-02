e0a639175d8fd263689dc99a1582053aa089472e
Heberth Lopez <heberth.lopez.19@gmail.com>
2023-06-01 20:28:28 -0500

    Added contact endpoint

--- src/index.js
+++ src/index.js
@@ -32,6 +32,29 @@ contactEmail.verify((error) => {
   }
 });
 
+app.post("/contact", (req, res) => {
+  const name = req.body.firstName + " " + req.body.lastName;
+  const email = req.body.email;
+  const message = req.body.message;
+  const phone = req.body.phone;
+  const mail = {
+    from: name,
+    to: process.env.EMAIL_ADDRESS,
+    subject: "Contact Form Submission - Portfolio",
+    html: `<p>Name: ${name}</p>
+    <p>Email: ${email}</p>
+    <p>Phone: ${phone}</p>
+    <p>Message: ${message}</p>`,
+  };
+  contactEmail.sendMail(mail, (error) => {
+    if (error) {
+      res.json(error);
+    } else {
+      res.json({ code: 200, status: "Message sent" });
+    }
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
 });
