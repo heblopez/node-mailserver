03afc5d2ee1b870a61330beb65fe111b4ec70bba
Heberth Lopez <heberth.lopez.19@gmail.com>
2023-06-01 20:04:25 -0500

    Added Gmail nodemailer config

--- src/index.js
+++ src/index.js
@@ -1,15 +1,35 @@
 const express = require("express");
-const path = require("path");
 const cors = require("cors");
+const nodemailer = require("nodemailer");
+require("dotenv").config();
 
 const PORT = process.env.PORT || 3001;
+// For SSL error
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 app.use(require("./routes/index"));
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
+});
+
+const contactEmail = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_ADDRESS,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+contactEmail.verify((error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Ready to send...");
+  }
 });
 
 app.listen(PORT, () => {
